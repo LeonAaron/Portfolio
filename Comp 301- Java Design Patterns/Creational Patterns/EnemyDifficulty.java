f4fe6f9704c34fb1ffc06847c126a40eb93d67e8
Leon Aaron <leoa@ad.unc.edu>
2026-03-10 10:39:29 -0400

    Create EnemyDifficulty.java

---
+++ Comp 301- Java Design Patterns/Creational Patterns/EnemyDifficulty.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+public enum EnemyDifficulty {
+    /**
+     * Beginner-friendly enemies.
+     */
+    EASY,
+    /**
+     * Standard difficulty enemies.
+     */
+    MEDIUM,
+    /**
+     * High difficulty enemies.
+     */
+    HARD
+}
